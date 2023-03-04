fb4f8c4bf85566213a2e688012fa550ccc0b628c
nysanier <nysanier@163.com>
2023-03-05 00:49:36 +0800

    修复编译docker问题

--- src/svcs/echo-svc/build_linux.sh
+++ src/svcs/echo-svc/build_linux.sh
@@ -11,4 +11,4 @@ echo "app_ver: ${app_ver}"
 echo "git_commit: ${git_commit}"
 echo "build_time: ${build_time}"
 ldflags="-X '${app_ver_key}=${app_ver}' -X '${git_commit_key}=${git_commit}' -X '${build_time_key}=${build_time}'"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "${ldflags}" -v -o docker-dir/echo-svc.bin main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "${ldflags}" -v -o z-docker-dir/echo-svc.bin main.go
